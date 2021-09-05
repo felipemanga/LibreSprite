35a550d54d713efee3817cb5c7907e7661532ee5
Felipe Manga <lufeboma@gmail.com>
2021-09-05 18:58:17 +0100

    Register duktape as a primary engine for duk files, secondary for js

--- src/script/duktape/engine.cpp
+++ src/script/duktape/engine.cpp
@@ -147,7 +147,7 @@ public:
   }
 };
 
-static Engine::Regular<DukEngine> registration("js");
+static Engine::Regular<DukEngine> registration("duk", {"js"});
 
 class DukScriptObject : public InternalScriptObject {
 public:
